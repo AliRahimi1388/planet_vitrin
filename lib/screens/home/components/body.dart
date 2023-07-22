ec4ae7ecc6db0eaab2e7a79145edf79c65b2930a
ali rahimi <ali.rahimi@divar.ir>
2023-07-22 20:42:17 +0330

    Create header and search box file

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:planet_app/constants.dart';
+import 'package:planet_app/screens/home/components/header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -13,87 +12,6 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-        ],
-      ),
-    );
-  }
-}
-
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    super.key,
-    required this.size,
-  });
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: size.height * 0.2,
-      child: Stack(
-        children: [
-          Container(
-            padding: EdgeInsets.only(
-                left: kDefaultPaddding,
-                right: kDefaultPaddding,
-                bottom: 36 + kDefaultPaddding),
-            height: size.height * 0.2 - 27,
-            decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36))),
-            child: Row(
-              children: [
-                Text(
-                  "Happy to See you",
-                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                Spacer(),
-                Image.asset("assets/images/logo.png"),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: kDefaultPaddding),
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPaddding),
-              height: 54,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.55)),
-                  ]),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(
-                          color: kPrimaryColor.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  SvgPicture.asset("assets/icons/search.svg")
-                ],
-              ),
-            ),
-          ),
         ],
       ),
     );
