a66f09a00bbbdb284f3b99150aed8a77c30b337a
ali rahimi <ali.rahimi@divar.ir>
2023-07-22 23:47:57 +0330

    Impl TitleWithCustomUnderline widget

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planet_app/constants.dart';
 import 'package:planet_app/screens/home/components/header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
@@ -12,8 +13,46 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
+          TitleWithCustomUnderline(
+            text: "Recomended",
+          )
         ],
       ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.only(left: kDefaultPaddding / 4),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            margin: EdgeInsets.only(right: kDefaultPaddding / 4),
+            height: 7,
+            color: kPrimaryColor.withOpacity(0.2),
+          ),
+        ),
+      ]),
     );
   }
 }
