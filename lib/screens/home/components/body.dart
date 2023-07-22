deeebf7927a615e0123170f33943bb6b201917d9
ali rahimi <ali.rahimi@divar.ir>
2023-07-23 00:43:34 +0330

    Fix files of widgets

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:planet_app/constants.dart';
 import 'package:planet_app/screens/home/components/header_with_searchbox.dart';
+import 'package:planet_app/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -16,44 +16,6 @@ class Body extends StatelessWidget {
           TitleWithMoreBtn(
             title: "Recomended",
             press: () {},
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class TitleWithMoreBtn extends StatelessWidget {
-  const TitleWithMoreBtn({
-    super.key,
-    required this.title,
-    required this.press,
-  });
-
-  final String title;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddding),
-      child: Row(
-        children: <Widget>[
-          TitleWithCustomUnderline(
-            text: title,
-          ),
-          Spacer(),
-          TextButton(
-            onPressed: press,
-            child: Text(
-              "More",
-              style: TextStyle(color: Colors.white),
-            ),
-            style: TextButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                backgroundColor: kPrimaryColor,
-                padding: EdgeInsets.all(0)),
           )
         ],
       ),
